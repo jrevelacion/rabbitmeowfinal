929d4de78e8a9b58f78a4e2af0126668d296b230
jrevelacion <3.6299926e+07+jrevelacion@users.noreply.github.com>
2026-07-05 12:06:55 +0000

    Fix index.css resolution in main.tsx files

--- src/utils/main.tsx
+++ src/utils/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import '../index.css'
 
 /* ------------------------------
    Clear browser storage ONLY when version changes
